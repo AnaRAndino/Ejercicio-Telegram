a6009929f29415c54f3cbfc76f201c65dc4b6def
AnaRAndino <aniandino58@gmail.com>
2019-02-10 22:14:56 -0600

    ++
    ++

---
+++ Telegram/public/js/telegram.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".chatButton").click(function(){
+        // $(this).css("background","rgb(86, 130, 163)");
+        $(this).hide();
+    });
+
+    // $("#dropdownMenuLink").click(function(){
+    //     $(".dropdown-menu").show();
+    // })
+ 
+})
